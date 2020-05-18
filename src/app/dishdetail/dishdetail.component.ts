4bc78fd75c4fa97f5b1e3abe5a84ed9e93e8d060
ThiliniDBandara <thilinidinushikabandara@gmail.com>
2020-05-18 21:17:14 +0530

    first commit

---
+++ src/app/dishdetail/dishdetail.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Dish } from '../shared/dish';
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { DishService } from '../services/dish.service';
+
+@Component({
+  selector: 'app-dishdetail',
+  templateUrl: './dishdetail.component.html',
+  styleUrls: ['./dishdetail.component.scss']
+})
+export class DishdetailComponent implements OnInit {
+
+  dish: Dish;
+  constructor(private dishService: DishService,
+    private router: ActivatedRoute,
+    private location: Location) { }
+
+  ngOnInit(): void { 
+    let id = this.router.snapshot.params['id'];
+    this.dish = this.dishService.getDish(id);
+  }
+  goBack(): void {
+    this.location.back();
+  }
+}
