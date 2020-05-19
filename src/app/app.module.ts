d14eb6680a041973a292b39a6487d46fdd49a911
ThiliniDBandara <thilinidinushikabandara@gmail.com>
2020-05-19 16:43:15 +0530

    update2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,9 +23,10 @@ import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
-
+import { LeaderService } from './services/leader.service';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { from } from 'rxjs';
 
 
 @NgModule({
@@ -50,7 +51,7 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     MatButtonModule,
     AppRoutingModule
   ],
-  providers: [DishService,PromotionService],
+  providers: [DishService,PromotionService,LeaderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -21,5 +21,4 @@ export class MenuComponent implements OnInit {
   onSelect(dish: Dish) {
     this.selectedDish = dish;
   }
-
 }
