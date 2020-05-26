49bc00beae857eec55a460f586eef7a2f4885ac4
ThiliniDBandara <thilinidinushikabandara@gmail.com>
2020-05-26 18:05:16 +0530

    comment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,81 +1,89 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar'; 
-import { FlexLayoutModule } from '@angular/flex-layout';
-
+import { BrowserModule, HammerModule } from '@angular/platform-browser';
+import{ NgModule } from '@angular/core';
+import{BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import{MatToolbarModule} from '@angular/material/toolbar';
+import{FlexLayoutModule} from '@angular/flex-layout';
+import {MatListModule} from '@angular/material/list';
+import{MatSelectModule} from '@angular/material/select';
+import{MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatGridListModule} from '@angular/material/grid-list';
+import{MatCardModule} from '@angular/material/card';
+import{MatButtonModule} from '@angular/material/button';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import{MatInputModule} from '@angular/material/input';
+import{MatCheckboxModule} from '@angular/material/checkbox';
+import {FormsModule} from '@angular/forms';
+import {ReactiveFormsModule} from '@angular/forms';
+import{MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import{MatSliderModule} from '@angular/material/slider';
 import { AppComponent } from './app.component';
 
+
 import 'hammerjs';
-import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
-
-import { MatListModule } from '@angular/material/list';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle'; 
-import { ReactiveFormsModule } from '@angular/forms';
-import { FormsModule } from '@angular/forms';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-
+import { MenuComponent } from './menu/menu.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 
-import { DishService } from './services/dish.service';
-import { PromotionService } from './services/promotion.service';
+
+import { from } from 'rxjs';
+
+import{DishService} from './services/dish.service';
+import{PromotionService} from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
 
-import { AppRoutingModule } from './app-routing/app-routing.module';
-import { from } from 'rxjs';
+import {AppRoutingModule} from './app-routing/app-routing.module';
 import { LoginComponent } from './login/login.component';
+import { importExpr } from '@angular/compiler/src/output/output_ast';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent,
     DishdetailComponent,
+    MenuComponent,
     HeaderComponent,
     FooterComponent,
-    AboutComponent,
     HomeComponent,
+    AboutComponent,
     ContactComponent,
     LoginComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    FlexLayoutModule,
     MatListModule,
+    MatSelectModule,
+    MatSlideToggleModule,
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
     MatDialogModule,
     MatFormFieldModule,
+    MatProgressSpinnerModule,
+    MatSliderModule,
     MatInputModule,
     MatCheckboxModule,
     FormsModule,
     ReactiveFormsModule,
-    AppRoutingModule,
-    MatSelectModule,
-    MatSlideToggleModule,
-    MatProgressSpinnerModule
+    FlexLayoutModule
+    
   ],
-  providers: [DishService,PromotionService,LeaderService],
-entryComponents: [
-  LoginComponent
-],
-
+  providers: [
+    DishService,
+    PromotionService,
+    LeaderService
+  ],
+  entryComponents:[
+    LoginComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,40 +1,124 @@
-import { Component, OnInit } from '@angular/core';
-import { Dish } from '../shared/dish';
-import { Params, ActivatedRoute } from '@angular/router';
-import { Location } from '@angular/common';
-import { DishService } from '../services/dish.service';
-import { switchMap } from 'rxjs/operators';
+import { Component, OnInit, ViewChild} from '@angular/core';
+import{Params, ActivatedRoute} from '@angular/router';
+import {Location} from '@angular/common';
+import {Dish} from '../shared/dish';
+import {DishService} from '../services/dish.service';
+import { of, from } from 'rxjs';
+import{delay, switchMap} from 'rxjs/operators';
+import{Comment} from  '../shared/comment';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss']
 })
-export class DishdetailComponent implements OnInit {
 
-  dish: Dish;
+export class DishdetailComponent implements OnInit {
+  
+  dish : Dish;
   dishIds: string[];
   prev: string;
   next: string;
-
+  @ViewChild('cform') commentFormDirective;
+  commentForm:FormGroup;
+  comment:Comment;
+  formErrors={
+    'author': '',
+    'çomment':''
+  };
+  validationMessage={
+    'author':{
+      'required':'Author Name is Required.',
+      'minlength':'Author name must be a least 2 characters',
+      'maxlength':'Author Name cannot be more than 25 charactes'
+    },
+    'comment':{
+      'required':'Comment is Required.',
+    
+    }
+  };
+  
   constructor(private dishService: DishService,
-    private router: ActivatedRoute,
-    private location: Location) { }
+    private route:ActivatedRoute,
+    private location:Location,
+    private fb:FormBuilder,
+   
+    ) {
+      
+     }
 
-  ngOnInit(): void { 
-    this.dishService.getDishIds()
-    .subscribe(dishIds => this.dishIds = dishIds);
-    this.router.params
-    .pipe(switchMap((params: Params) => this.dishService.getDish(params['id'])))
-    .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
-  }
-  setPrevNext(dishId: string) {
-    const index = this.dishIds.indexOf(dishId);
-    this.prev = this.dishIds[(this.dishIds.length + index - 1) % this.dishIds.length];
-    this.next = this.dishIds[(this.dishIds.length + index + 1) % this.dishIds.length];
-  }
+  
 
-  goBack(): void {
+  ngOnInit(): void {
+
+    
+
+    this.createForm();
+    
+     this.dishService.getDishIds()
+    .subscribe((dishIds)=>this.dishIds=dishIds);
+     this.route.params.
+     pipe(switchMap((params:Params)=>this.dishService.getDish(params['id'])))
+  
+    .subscribe(dish=>{this.dish=dish; this.setPrevNext(dish.id); });
+  }
+  setPrevNext(dishId:string){
+    const index=this.dishIds.indexOf(dishId);
+    this.prev=this.dishIds[(this.dishIds.length + index -1)%this.dishIds.length];
+    this.next=this.dishIds[(this.dishIds.length + index +1)%this.dishIds.length];
+  
+  }
+  goBack():void{
     this.location.back();
   }
+  createForm(){
+    this.commentForm=this.fb.group({
+      author:['',[Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      rating:5,
+      comment:['',[Validators.required, Validators.minLength(2)]]
+
+    });
+    this.commentForm.valueChanges
+    .subscribe(data=>this.onValueChanged(data));
+    this.onValueChanged();
+  }
+  onValueChanged(data?: any){
+    if(!this.commentForm){return;}
+    const form= this.commentForm;
+    for(const field in this.formErrors){
+      if(this.formErrors.hasOwnProperty(field)){
+        this.formErrors[field]='';
+        const control= form.get(field);
+        if(control && control.dirty && !control.valid){
+          const messages= this.validationMessage[field];
+          for(const key in control.errors){
+            if(control.errors.hasOwnProperty(key)){
+              this.formErrors[field]+=messages[key] + '';
+            }
+          }
+        }
+      }
+    }
+  }
+  onSubmit(){
+    this.comment=this.commentForm.value;
+    this.comment.date=new Date().toISOString();
+    console.log(this.comment);
+    this.dish.comments.push(this.comment);
+    this.commentForm.reset({
+      author:'',
+      rating:'',
+      comment:''
+    
+
+    });
+  
+   
+     this.commentFormDirective.resetForm();
+  }
+
+  
+
 }
